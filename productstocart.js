9c1e593e10dbfc01a5acb0c8e748382a1c1b95d2
banglaploa@gmail.com <banglaploa@gmail.com>
2023-07-13 01:56:22 +0600

    iei

--- productstocart.js
+++ productstocart.js
@@ -20,7 +20,7 @@
         const quantityValue = quantityInput.value;
         const detailsValue = detailsInput.value;
 
-        if (quantityValue === '' || detailsValue === '') {
+        if (quantityValue === '' || quantityValue === '0' || detailsValue === '') {
           return;
         }
 
