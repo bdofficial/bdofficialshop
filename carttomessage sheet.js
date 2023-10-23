fb428d643f3415c9549453ef5422a4ca7b51d07b
banglaploa@gmail.com <banglaploa@gmail.com>
2023-10-23 12:05:56 +0600

    yyttt

---
+++ carttomessage sheet.js
@@ -0,0 +1,35 @@
+//////////////cart to message 
+function copyTextarea() {
+  const roomInput = document.getElementById("room");
+
+  items.forEach(function(item) {
+    const details = item.item + " " + item.price + "TK";
+    const quantity = item.quantity;
+    const total = calculateItemTotal(quantity, item.price) + "TK";
+    roomInput.value += "✓" + "Details" + details + "," + "Quantity: " + quantity + "," + "Price: " + total + ".";
+  });
+
+  if (items.length > 1) {
+    roomInput.value += document.getElementById("cart").value;
+  }
+  var input = document.getElementById("room").value;
+  document.getElementById("my-input").value += input;
+  submitMessage();
+  document.getElementById("room").value = "";
+}
+/////
+///////////iframe chat submit & scrool
+function submitMessage() {
+  const iframe = document.getElementById('chatiframe');
+  const nam = document.getElementById("name").value;
+  const mes = document.getElementById('my-input').value;
+  if (mes === ' ' || mes === '') {
+    return;
+  }
+  const message = "𖦹 " + mes;
+  iframe.contentWindow.postMessage({ message }, '*');
+  document.getElementById('my-input').value = "";
+}
+///////
+
+
---
+++ iframe sheet.js
@@ -0,0 +1,51 @@
+      function sendMessage() {
+        const chatiframe = document.getElementById('chatiframe');
+        const message = {
+          name: document.getElementById("name").value,
+          address: document.getElementById("address").value,
+          number: document.getElementById("number").value
+        };
+        // Set cookies with message data
+        document.cookie = "name=" + message.name + "; max-age=157784630; secure";
+        document.cookie = "address=" + message.address + "; max-age=157784630; secure";
+        document.cookie = "number=" + message.number + "; max-age=157784630; secure";
+        // Post message to iframe
+        chatiframe.contentWindow.postMessage(message, "*");
+      }
+
+      function receiveMessage(event) {
+  const data = event.data;
+  document.cookie = "name=" + encodeURIComponent(data.name);
+  document.cookie = "address=" + encodeURIComponent(data.address);
+  document.cookie = "number=" + encodeURIComponent(data.number);
+  updateInputFields();
+}
+
+
+      function updateInputFields() {
+        const name = document.cookie.replace(/(?:(?:^|.*;\s*)name\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+        const address = document.cookie.replace(/(?:(?:^|.*;\s*)address\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+        const number = document.cookie.replace(/(?:(?:^|.*;\s*)number\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+        document.getElementById("name").value = name;
+        document.getElementById("address").value = address;
+        document.getElementById("number").value = number;
+        ////////update iframe 
+        document.getElementById("sendcookies").click();
+        //////
+      }
+      
+      window.addEventListener("message", receiveMessage, false);
+ /////////onload
+ window.onload = function () {
+ updateInputFields();
+ };
+ //////
+ ///////on click scroll
+ const chatiframe = document.getElementById('chatiframe');
+ const messa = document.getElementById('messa');
+ messa.addEventListener('click', function() {
+   setTimeout(function() {
+     chatiframe.contentWindow.postMessage('scrollToBottom', '*');
+   },300);
+ });
+////////////
