84138c19763734ad3bc078113b3d3826b6eeb3fa
banglaploa@gmail.com <banglaploa@gmail.com>
2023-11-14 11:08:10 +0600

    uuu

--- carttomessage.js
+++ carttomessage.js
@@ -1,16 +1,5 @@
 //////////////cart to message 
 function copyTextarea() {
-  const roomInput = document.getElementById("room");
-  items.forEach(function(item) {
-    const details = item.item + " " + item.price + "TK";
-    const quantity = item.quantity;
-    const total = calculateItemTotal(quantity, item.price) + "TK";
-    roomInput.value += "✓" + "Details" + details + "," + "Quantity: " + quantity + "," + "Price: " + total + ".";
-  });
-
-  if (items.length > 1) {
-    roomInput.value += document.getElementById("cart").value;
-  }
   var input = document.getElementById("room").value;
   document.getElementById("my-input").value += input;
   submitMessage();
